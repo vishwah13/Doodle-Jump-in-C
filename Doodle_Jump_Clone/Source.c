76b9b393663fb43c0f56f9a34ed5fe448567dd67
vishwahrocks <vishwahrocks@gmail.com>
2022-07-23 22:24:59 +0530

    more refactoring

--- Doodle_Jump_Clone/Source.c
+++ Doodle_Jump_Clone/Source.c
@@ -1,5 +1,5 @@
 #include"raylib.h"
-#include "raymath.h"
+#include"raymath.h"
 
 #define G 400
 #define PLAYER_JUMP_SPD 350.0f
@@ -9,6 +9,7 @@
 
 typedef struct Player {
 	Vector2 position;
+	Rectangle PlayerRect;
 	float speed;
 	bool canJump;
 } Player;
@@ -19,11 +20,10 @@ typedef struct Platform {
 	Color color;
 } Platform;
 
-Rectangle playerRect;
 
 Platform platforms[PLATFORMCOUNT];
-int y = 800;
-int platformsLength;
+int GeneratedPlatformYaxisValue = 800;
+int PlatformsLength;
 
 int Score;
 bool bGameOver = false;
@@ -47,17 +47,12 @@ int main()
 {
 	const int ScreenWidth = 400;
 	const int ScreenHeight = 600;
-
-
 	InitWindow(ScreenWidth, ScreenHeight, "Doodle Jump Clone");
 
 	Player player = { 0 };
-	
 	Camera2D camera = { 0 };
-
 	InitGame(&player, &camera);
 	
-
 	while (!WindowShouldClose())
 	{
 		float deltaTime = GetFrameTime();
@@ -68,13 +63,13 @@ int main()
 			
 			if (!bPaused)
 			{
-				UpdatePlayer(&player, platforms, platformsLength, deltaTime);
+				UpdatePlayer(&player, platforms, PlatformsLength, deltaTime);
 
-				UpdateCameraEvenOutOnLanding(&camera, &player, platforms, platformsLength, deltaTime, ScreenWidth, ScreenHeight);
+				UpdateCameraEvenOutOnLanding(&camera, &player, platforms, PlatformsLength, deltaTime, ScreenWidth, ScreenHeight);
 
 				UpdateScore(&player);
 
-				DestroyPlatformAfterCrossedCameraBorder(platforms, platformsLength, &camera);
+				DestroyPlatformAfterCrossedCameraBorder(platforms, PlatformsLength, &camera);
 				CheckForGameOver(&player, &camera);
 			}
 		}
@@ -87,35 +82,35 @@ int main()
 			}
 		}
 
-		//playerRect = { player.position.x - 20, player.position.y - 40, 40, 40 };
-		playerRect.x = player.position.x - 20;
-		playerRect.y = player.position.y - 40;
-		playerRect.width = 40;
-		playerRect.height = 40;
+		player.PlayerRect.x = player.position.x - 20;
+		player.PlayerRect.y = player.position.y - 40;
+		player.PlayerRect.width = 40;
+		player.PlayerRect.height = 40;
 
-		if (player.position.y < y)
+		if (player.position.y < GeneratedPlatformYaxisValue)
 		{
-			y += 500;
+			GeneratedPlatformYaxisValue += 500;
 			RandomPlatformGenerator();
 		}
 
+		//Drawing Things
 		BeginDrawing();
+		ClearBackground(LIGHTGRAY);
+		BeginMode2D(camera);
+		//Drawing Platforms
+		for (int i = 0; i < PlatformsLength; i++)
+		{
+			DrawRectangleRec(platforms[i].rect, platforms[i].color);
+		}
+		//Drawing Player
+		DrawRectangleRec(player.PlayerRect, RED);
+		EndMode2D();
 		//Drawing Score Text
 		DrawText(TextFormat("%08i", Score), 5, 5, 20, RED);
-
 		if (bGameOver)
-			DrawText(TextFormat("Press ENTER to Play again !"), 80, 250, 20, RED);
-		
-		ClearBackground(LIGHTGRAY);
-		
-		BeginMode2D(camera);
-
-		//Drawing Platforms
-		for (int i = 0; i < platformsLength; i++) DrawRectangleRec(platforms[i].rect, platforms[i].color);
-
-		//Drawing Player
-		DrawRectangleRec(playerRect, RED);
-
+		{
+			DrawText(TextFormat("Press ENTER to Play again !"), 50, 250, 20, RED);
+		}
 		EndDrawing();
 	}
 	CloseWindow();
@@ -124,7 +119,7 @@ int main()
 
 void InitGame(Player* player, Camera2D* camera)
 {
-	y = 800;
+	GeneratedPlatformYaxisValue = 800;
 	Score = 0;
 
 	player->position = (Vector2){ 300, 400 };
@@ -140,8 +135,14 @@ void InitGame(Player* player, Camera2D* camera)
 
 void UpdatePlayer(Player* player, Platform* platform, int platformsLength, float delta)
 {
-	if (IsKeyDown(KEY_LEFT)) player->position.x -= PLAYER_HOR_SPD * delta;
-	if (IsKeyDown(KEY_RIGHT)) player->position.x += PLAYER_HOR_SPD * delta;
+	if (IsKeyDown(KEY_LEFT))
+	{
+		player->position.x -= PLAYER_HOR_SPD * delta;
+	}
+	if (IsKeyDown(KEY_RIGHT))
+	{
+		player->position.x += PLAYER_HOR_SPD * delta;
+	}
 	if (player->canJump)
 	{
 		player->speed = -PLAYER_JUMP_SPD;
@@ -163,7 +164,6 @@ void UpdatePlayer(Player* player, Platform* platform, int platformsLength, float
 			player->speed = 0.0f;
 			p->y = ei->rect.y;
 		}
-		
 	}
 
 	if (!hitObstacle)
@@ -172,7 +172,10 @@ void UpdatePlayer(Player* player, Platform* platform, int platformsLength, float
 		player->speed += G * delta;
 		player->canJump = false;
 	}
-	else player->canJump = true;
+	else
+	{
+		player->canJump = true;
+	}
 
 	//Invert the Player postion on X Axis While player cross the border on X Axis
 	if (player->position.x > GetScreenWidth()+100 ) player->position.x = 40;
@@ -223,11 +226,10 @@ void UpdateCameraEvenOutOnLanding(Camera2D* camera, Player* player, Platform* pl
 
 void RandomPlatformGenerator()
 {
-	
 	for (int i = 0;i < PLATFORMCOUNT;i++)
 	{
 		Rectangle RandomRec[PLATFORMCOUNT];
-		Rectangle tempRandomRec = { GetRandomValue(0,400), y -= 50,50,10 };
+		Rectangle tempRandomRec = { GetRandomValue(0,400), GeneratedPlatformYaxisValue -= 50,50,10 };
 		RandomRec[i] = tempRandomRec;
 		
 		platforms[i].rect = RandomRec[i];
@@ -235,7 +237,7 @@ void RandomPlatformGenerator()
 		platforms[i].color = YELLOW;
 	}
 
-	platformsLength = sizeof(platforms) / sizeof(platforms[0]);
+	PlatformsLength = sizeof(platforms) / sizeof(platforms[0]);
 }
 
 void DestroyPlatformAfterCrossedCameraBorder(Platform* platform, int platformsLength, Camera2D* camera)
@@ -246,7 +248,6 @@ void DestroyPlatformAfterCrossedCameraBorder(Platform* platform, int platformsLe
 		{
 			platform[i] = platform[i + 1];
 		}
-			
 	}
 }
 
@@ -261,6 +262,7 @@ void CheckForGameOver(Player* player, Camera2D* camera)
 void UpdateScore(Player* player)
 {
 	if (player->canJump && (player->speed == 0))
-		Score = (player->position.y - 600)  * -1;
-	
+	{
+		Score = (player->position.y - 600) * -1;
+	}
 }
